15e728f6fe0851768b275da845dac89ee579f9d0
Connor Hanley <connordhanley@gmail.com>
2025-12-09 10:35:35 -0500

    refactor: implemented activity product rule model, preliminary testing in main

--- wirex/__main__.py
+++ wirex/__main__.py
@@ -1,50 +1,60 @@
 """Developmental testing of the wirex program."""
 
+import jax
 import jax.numpy as jnp
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from einops import rearrange
+from numpy.typing import NDArray
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
+
+from wirex.models.apr import ActivityProductRule
 
 from .models.hopfield import Hopfield
 
 matplotlib.use("Qt5Agg")
 
-DATA_CACHE = "./cache/mnist/"
+DATA_CACHE = "./.cache/mnist/"
 pxw, pxh = 28, 28
 
 
-def transform(data):
-    data = np.array(data, dtype=jnp.float32)
-    data = rearrange(data, "w h -> (w h)")
-    data[data > 0.0] = 1.0
-    data[data == 0.0] = -1.0
-    return data
+def transform(data: torch.Tensor) -> NDArray[np.float32]:
+    dataarr: NDArray[np.float32] = np.array(data, dtype=jnp.float32)
+    dataarr = rearrange(dataarr, "w h -> (w h)")
+    dataarr[dataarr > 0.0] = 1.0
+    dataarr[dataarr == 0.0] = -1.0
+    return dataarr
 
 
-mnist_train = MNIST(DATA_CACHE, train=True, transform=transform, download=True)
-mnist_data_loader = DataLoader(mnist_train, batch_size=128, shuffle=True)
-mnist_it = iter(mnist_data_loader)
-mnist_data, _ = next(mnist_it)
-
-
-def show_im(im, title=""):
+def show_im(im: torch.Tensor | jax.Array, title: str = "") -> None:
     im = rearrange(im, "(w h) -> w h", w=pxw, h=pxh)
-    axesim = plt.imshow(im)
+    plt.imshow(im)
     plt.title(title)
     plt.xticks([])
     plt.yticks([])
-    return axesim
 
 
-# show_im(mnist_data[0], title="Testing whether the transform is correct")
-# plt.show()
+def main() -> None:
+    mnist_train = MNIST(DATA_CACHE, train=True, transform=transform, download=True)
+    mnist_data_loader = DataLoader(mnist_train, batch_size=128, shuffle=True)
+    mnist_it = iter(mnist_data_loader)
+    mnist_data, _ = next(mnist_it)
 
-Xi = jnp.array(mnist_data[:2], dtype=jnp.float32)
-hopfield = Hopfield(Xi.T @ Xi)
-query = Xi[1]
-result = hopfield.recall(query)
-show_im(result)
-plt.show()
+    Xi = jnp.array(mnist_data[:2], dtype=jnp.float32)
+    hopfield = Hopfield(Xi.T @ Xi)
+    query = Xi[1]
+    result = hopfield.recall(query)
+    show_im(result, "Hopfield_result")
+    plt.show()
+
+    apr = ActivityProductRule.init(Xi.shape[-1], 1).fit(Xi)
+    result = apr.recall(query)
+    show_im(result, "APR Result")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ wirex/models/apr.py
@@ -0,0 +1,99 @@
+"""Implementation of an [Activity Product Rule](https://neuronaldynamics.epfl.ch/online/Ch19.S2.html)."""
+
+from typing import Self, override
+
+import jax.lax as lax
+import jax.numpy as jnp
+import numpy as np
+from jaxtyping import Array, Float
+
+from .coeffs import Coefficients
+from .hebbian import Hebbian
+
+__all__ = ["ActivityProductRule"]
+
+
+class ActivityProductRule(Hebbian):
+    r"""Activity Product Rule Hebbian associative memory.
+
+    The activity product learning rule is defined as:
+    $$
+    \Delta W = \eta x x^\top,
+    $$
+    where $W$ is the weights of the model, $x$ is the pattern that
+    you wish to memorize, and $\eta$ the learning rate.
+
+    Example:
+        `ActivityProductRule` is initializes with a the class method `ActivityProductRule.init`,
+        and then fit to a list of patterns using `ActivityProductRule.fit`.
+        ```
+        patterns = ...
+        pattern_dim = patterns.shape[-1]
+        apr = ActivityProductRule.init(pattern_dim, learning_rate=1).fit(patterns)
+        ```
+    """
+
+    weights: Float[Array, "D D"]
+    coefficients: Coefficients
+
+    @classmethod
+    def init(cls, pattern_dim: int, learning_rate: float) -> Self:
+        """Initialize a new `ActivityProductRule`.
+
+        Args:
+            pattern_dim: The dimensionality of the patterns that are to be stored.
+            learning_rate: The learning rate used in in the learning rule.
+        """
+        weights: Float[Array, "D D"] = jnp.zeros(
+            (pattern_dim, pattern_dim), dtype=jnp.float32
+        )
+        coefficients = Coefficients.init(c_2_corr=learning_rate)
+        return cls(weights=weights, coefficients=coefficients)
+
+    def fit(self, patterns: Float[Array, "N D"]) -> Self:
+        """Fit an initialied `ActivityProductRule` model to a pattern set.
+
+        Args:
+            patterns:
+                `(N, D)`-dimensional matrix of `N` patterns, where `D` is `pattern_dim` used in initialization.
+
+        Returns:
+            A new reference to an `ActivityProductRule` model with the weights updated
+            according to `ActivityProductRule.learning_rule`.
+        """
+
+        def update_step(
+            weights: Float[Array, "D D"], i: np.ndarray
+        ) -> tuple[Float[Array, "D D"], None]:
+            pattern = patterns[i]
+            updated_weights = self.learning_rule(
+                weights, self.coefficients, pattern, pattern
+            )
+            return updated_weights, None
+
+        n = patterns.shape[0]
+
+        final_weights, _ = lax.scan(update_step, init=self.weights, xs=np.arange(n))
+        return self.__class__(weights=final_weights, coefficients=self.coefficients)
+
+    @override
+    @staticmethod
+    def learning_rule(
+        weights: Float[Array, "D D"],
+        coefficients: Coefficients,
+        pre_synaptic_layer: Float[Array, "D"],
+        post_synaptic_layer: Float[Array, "D"],
+    ) -> Float[Array, "D D"]:
+        r"""Activity Product Rule learning rule:
+
+        $$
+        \Delta W = \eta x x^\top,
+        $$
+        """
+        return weights + coefficients.c_2_corr * jnp.outer(
+            pre_synaptic_layer, post_synaptic_layer
+        )
+
+    @override
+    def recall(self, query: Float[Array, "D"]) -> Float[Array, "D"]:
+        return jnp.sign(self.weights.dot(query))
